cce7863f8da161a5d493cac6bc66a0add0d518cf
samir-massoundi <samir.j7.massoundi@gmail.com>
2022-03-06 17:28:26 +0100

    last tests

--- Back/tests/ApiTest.php
+++ Back/tests/ApiTest.php
@@ -17,7 +17,8 @@ class ApiTest extends WebTestCase
         $responseData = json_decode($response->getContent(), true);
         $this->assertEquals(['message' => "Hello world"], $responseData);
     }
-    public function testRetreiveProducts():void
+
+    public function testRetreiveProducts(): void
     {
         $client = static::createClient();
         // Request a specific page
@@ -31,15 +32,14 @@ class ApiTest extends WebTestCase
         $this->assertCount(20, $responseData);
 
         $element = $responseData[0];
-        $this->assertArrayHasKey('id',$element);
-        $this->assertArrayHasKey('name',$element);
-        $this->assertArrayHasKey('price',$element);
-        $this->assertArrayHasKey('quantity',$element);
-        $this->assertArrayHasKey('image',$element);
-
+        $this->assertArrayHasKey('id', $element);
+        $this->assertArrayHasKey('name', $element);
+        $this->assertArrayHasKey('price', $element);
+        $this->assertArrayHasKey('quantity', $element);
+        $this->assertArrayHasKey('image', $element);
     }
 
-    public function testRetreiveProduct():void
+    public function testRetreiveProduct(): void
     {
         $client = static::createClient();
         // Request a specific page
@@ -49,40 +49,77 @@ class ApiTest extends WebTestCase
         $this->assertJson($response->getContent());
         $responseData = json_decode($response->getContent(), true);
 
-        $this->assertIsArray($responseData,'check if its object or not');
+        $this->assertIsArray($responseData, 'check if its object or not');
 
 
-        $this->assertArrayHasKey('id',$responseData);
-        $this->assertArrayHasKey('name',$responseData);
-        $this->assertArrayHasKey('price',$responseData);
-        $this->assertArrayHasKey('quantity',$responseData);
-        $this->assertArrayHasKey('image',$responseData);
+        $this->assertArrayHasKey('id', $responseData);
+        $this->assertArrayHasKey('name', $responseData);
+        $this->assertArrayHasKey('price', $responseData);
+        $this->assertArrayHasKey('quantity', $responseData);
+        $this->assertArrayHasKey('image', $responseData);
     }
 
-    public function getCart():void
+    public function testGetCart(): void
     {
         $client = static::createClient();
         // Request a specific page
+        $client->jsonRequest('POST', 'api/cart/1', ["quantity" => 1]);
+
         $client->jsonRequest('GET', '/api/cart');
         $response = $client->getResponse();
         $this->assertResponseIsSuccessful();
         $this->assertJson($response->getContent());
         $responseData = json_decode($response->getContent(), true);
 
-        $this->assertIsArray($responseData,'check if its object or not');
-        $this->assertIsBool($responseData['products'][0],'check if its object or not');
+        // $this->assertIsArray($responseData, 'check if its object or not');
+        // $this->assertIsArray($responseData['products'][0], 'check if its object or not');
+        
+        // $this->assertArrayHasKey('id', $responseData['products'][0]);
+        // $this->assertArrayHasKey('name', $responseData['products'][0]);
+        // $this->assertArrayHasKey('price', $responseData['products'][0]);
+        // $this->assertArrayHasKey('quantity', $responseData['products'][0]);
+        // $this->assertArrayHasKey('image', $responseData['products'][0]);
     }
 
-    // public function addProductToCart() {
-    //     $client = static::createClient();
-    //     // Request a specific page
-    //     $client->jsonRequest('post', '/api/products/1');
-    // }
-
-    public function deleteCart():void
+    public function testAddProductToCart()
     {
         $client = static::createClient();
         // Request a specific page
+        $client->jsonRequest('POST', 'api/cart/1', ["quantity" => 1]);
+        $response = $client->getResponse();
+        $this->assertResponseIsSuccessful();
+        $this->assertJson($response->getContent());
+        $responseData = json_decode($response->getContent(), true);
+
+        $this->assertIsArray($responseData, 'check if its object or not');
+        $this->assertIsArray($responseData['products'][0], 'check if its object or not');
+        
+        $this->assertArrayHasKey('id', $responseData['products'][0]);
+        $this->assertArrayHasKey('name', $responseData['products'][0]);
+        $this->assertArrayHasKey('price', $responseData['products'][0]);
+        $this->assertArrayHasKey('quantity', $responseData['products'][0]);
+        $this->assertArrayHasKey('image', $responseData['products'][0]);
+    }
+
+    public function testAddProductToCartFail(): void 
+    {
+        $client = static::createClient();
+        // Request a specific page
+        $client->jsonRequest('POST', 'api/cart/1', ["quantity" => 9999]);
+        $response = $client->getResponse();
+        $this->assertResponseIsSuccessful();
+        $this->assertJson($response->getContent());
+        $responseData = json_decode($response->getContent(), true);
+        $this->assertEquals(['error' => "too many"], $responseData);
+
+    }
+
+    public function testDeleteProductFromCart(): void
+    {
+        $client = static::createClient();
+        // Request a specific page
+        $client->jsonRequest('POST', 'api/cart/1', ["quantity" => 1]);
+
         $client->jsonRequest('DELETE', '/api/cart/1');
         $response = $client->getResponse();
         $this->assertResponseIsSuccessful();
